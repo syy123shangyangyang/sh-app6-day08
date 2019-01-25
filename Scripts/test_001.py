0fc080aaded28c7f9d2296206b92a8f89ab49b7b
syy123shangyangyang <1335575187@qq.com>
2019-01-25 16:39:55 +0800

    第一个项目

---
+++ Scripts/test_001.py
@@ -0,0 +1,13 @@
+
+class Test_001:
+
+    def test_001_1(self):
+        print("test_001_1----")
+        assert True
+    def test_001_2(self):
+        print("test_001_2----")
+        assert False
+
+
+
+
